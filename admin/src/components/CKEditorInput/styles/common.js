08882b36bdd5f099f2b06db035f6c6e9ecc1fd7e
Bogdan Covrig <bogcov@gmail.com>
2023-08-11 14:23:45 +0200

    fix: Update editor css selector again

--- admin/src/components/CKEditorInput/styles/common.js
+++ admin/src/components/CKEditorInput/styles/common.js
@@ -112,7 +112,7 @@ export const style = css`
     }
 
     // higher specificity needed
-    &.ck-editor__editable:not(.ck-focused) { {
+    .ck.ck-content.ck-editor__editable:not(.ck-focused) {
       line-height: initial;
       min-height: 12.5rem;
 
